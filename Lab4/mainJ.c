d70627d7f5e4a347ff64f039843ec1cf5180be41
Jonathan Gustafson <jonathgu@kth.se>
2022-12-07 16:02:50 +0100

    update

--- Lab4/mainJ.c
+++ Lab4/mainJ.c
@@ -10,6 +10,32 @@ int start = 0;
 //list of requests
 int request_list[REQUESTS];
 
+int findHeadIndexAndSort(int *list, int size, int head){
+    //i starts on the 2nd element
+    for(int i = 1; i < size; i++) {
+        int current = list[i];
+        //System.out.println("Current: " + current);
+        
+        //j starts on the element to the left i
+        int j = i-1;
+        
+        //while the current item is less than j, move j one step to the right.
+        while(j >= 0 && list[j] > current) {
+            list[j + 1] = list[j]; //move the element on j one step to the right
+            j--;
+        }
+        
+        list[j+1] = current; //place current in the right slot
+    }
+    for (int i = 0; i < REQUESTS; i++)
+    {
+        if(start < list[i]){
+            return i;
+        }
+    }
+    
+}
+
 void sort(int *list, int size){
     //i starts on the 2nd element
 	for(int i = 1; i < size; i++) {
@@ -29,9 +55,8 @@ void sort(int *list, int size){
 	}
 }
 
-
-
-int sstf(int* request_list, int size){
+int sstf(int* request_list){
+    int size = REQUESTS;
     sort(request_list, size);
 
     int head_movement = 0;      //Total amount of head movements
@@ -103,17 +128,122 @@ int sstf(int* request_list, int size){
 
 }
 
+int fcfs(int *request_array){
+
+    int head_movement = 0;
+    int oneWay;
+    int anotherWay;
+    if(abs(request_array[0] - start) > abs(start - request_array[0])){
+        head_movement = abs(start - request_array[0]);
+    }else{
+        head_movement = abs(request_array[0] - start);
+    }
+
+   
+    for (int i = 1; i < REQUESTS; i++){
+    
+    oneWay = abs(request_array[i-1] - request_array[i]);
+    anotherWay = abs(request_array[i] - request_array[i-1]);
+    
+    if(anotherWay > oneWay){
+         head_movement = head_movement + oneWay;
+    }else{
+            head_movement = head_movement + anotherWay;
+        }
+ 
+    }
+    return head_movement;
+}
+
+int cscan(int * request_array) {  //should work
+    int head_movement = 0;
+    
+    int indexToStart = findHeadIndexAndSort(request_array, REQUESTS, start);
+    head_movement = request_array[indexToStart] - start;
+    for (int i = start; i < REQUESTS; i++){
+        head_movement = head_movement + request_array[i + 1] - request_array[i];
+    }  
+    head_movement = head_movement + (CYLINDERS - request_array[REQUESTS - 1]);
+    head_movement = head_movement + CYLINDERS;
+    for (int i = 0; i < indexToStart - 1; i++){
+        head_movement = head_movement + request_array[i + 1] - request_array[i];
+    }
+  return head_movement;
+
+}
+
+int clook(int * request_array) {  //should work
+    int head_movement = 0;
+   
+    int indexToStart = findHeadIndexAndSort(request_array, REQUESTS, start);
+    head_movement = request_array[indexToStart] - start;
+    for (int i = start; i < REQUESTS; i++){
+        head_movement = head_movement + request_array[i + 1] - request_array[i];
+    }  
+    
+    head_movement = head_movement + CYLINDERS - request_array[0];
+    for (int i = 0; i < indexToStart - 1; i++){
+        head_movement = head_movement + request_array[i + 1] - request_array[i];
+    }
+  return head_movement;
+  
+
+}
+
+int look(int * request_array) { //Should work
+
+    int head_movement = 0;
+    
+    int indexToStart = findHeadIndexAndSort(request_array, REQUESTS, start);
+    head_movement = request_array[indexToStart] - start;
+    for (int i = indexToStart; i < CYLINDERS; i++){
+        head_movement = head_movement + request_array[i + 1] - request_array[i];
+    }
+
+    
+    head_movement = head_movement + (CYLINDERS - request_array[indexToStart - 1]);
+    for (int i = indexToStart - 1; i > 0; i--){
+        head_movement = head_movement + request_array[i] - request_array[i - 1];
+    }    
+    return head_movement;
+
+}
+
+int scan(int * request_array) { //Should work
+
+    int head_movement = 0;
+    
+    int indexToStart = findHeadIndexAndSort(request_array, REQUESTS, start);
+    head_movement = request_array[indexToStart] - start;
+    for (int i = indexToStart; i < CYLINDERS; i++){
+        head_movement = head_movement + request_array[i + 1] - request_array[i];
+    }
+
+    head_movement = head_movement + (CYLINDERS - request_array[REQUESTS - 1]);
+    head_movement = head_movement + (CYLINDERS - request_array[indexToStart - 1]);
+    for (int i = indexToStart - 1; i > 0; i--){
+        head_movement = head_movement + request_array[i] - request_array[i - 1];
+    }    
+    return head_movement;
+
+}
+
 void test1(){
     for(int i = 0; i < REQUESTS; i++) {
 		request_list[i] = rand() % CYLINDERS;
 	}
-    printf("SSTF head movements: %d\n", sstf(request_list, REQUESTS));
-}
+    printf("FCFS \thead movements: %d\n", fcfs(request_list));
+    printf("SSTF \thead movements: %d\n", sstf(request_list));
+    printf("C-SCAN \thead movements: %d\n", cscan(request_list));
+    printf("C-LOOK \thead movements: %d\n", clook(request_list));
+    printf("LOOK \thead movements: %d\n", look(request_list));
+    printf("SCAN \thead movements: %d\n", scan(request_list));
+    }
 
 void test2(){
     //sorted we have     {14,37,65,67,98,122,124,183}
     int request_list[] = {98,183,37,122,14,124,65,67};
-    printf("\nSSTF head movements: %d\n", sstf(request_list, 8));
+    printf("\nSSTF head movements: %d\n", sstf(request_list));
 }
 
 int main (int argc, char *argv[]) {
@@ -125,8 +255,8 @@ int main (int argc, char *argv[]) {
         start = atoi(argv[1]);
     }
 
-    //test1();
-    test2();
+    test1();
+    //test2();
 
 	return 0;
 }
